9ba3656e7b2772d18b46f890ad9c860c3c0b6975
Ciobanu Nicolae <100871808+CiobanuNicolae777@users.noreply.github.com>
2022-03-05 01:18:17 +0200

    din zecimal in binar

--- EX_13.cpp
+++ EX_13.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+#include <math.h>
 
+using namespace std;
+
+void zecimal_in_binar(int n)
+{
+    int nr_binar[32];
+
+    int i = 0;
+
+    while (n > 0) {
+        nr_binar[i] = n % 2;
+        n = n / 2;
+        i++;
+    }
+
+    for (int j = i - 1; j >=0; j--)
+    cout << nr_binar[j];
+}
+
+int main()
+{
+    int n;
+    cout << "Dati numarul zecimal:  ";
+    cin >> n;
+    zecimal_in_binar(n);
+    return 0;
+}
